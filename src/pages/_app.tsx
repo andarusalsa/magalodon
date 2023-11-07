f8ee03abb07aefb489a4e68ea5434afaae7c0fd0
andarusala <salsa34545@gmail.com>
2023-11-08 02:21:09 +0700

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,9 @@
-//import '@/styles/globals.css'
 import Appshell from "@/components/layout/Appshell";
-import { faBullseye } from "@fortawesome/free-solid-svg-icons";
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Appshell withHeaderAndFooter={false}>
       <Component {...pageProps} />
-    </Appshell>
   ) ;
   
 }
